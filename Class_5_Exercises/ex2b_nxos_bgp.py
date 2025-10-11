ac26fb41378c64f7491a30019b04e04555ce1a79
Joel Lawless <jlawless6@gmail.com>
2025-10-10 19:01:47 -0700

    Completed Class 5 Exercise 2b

--- Class_5_Exercises/ex2b_nxos_bgp.py
+++ Class_5_Exercises/ex2b_nxos_bgp.py
@@ -4,12 +4,18 @@ from jinja2.environment import Environment
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader('./templates/exercise2')
 
-router_vars = {
-    "nxos1": {'interface': "Ethernet1/1", 'ip': "10.1.100.1", 'netmask': "24"},
-    "nxos2": {'interface': "Ethernet1/1", 'ip': "10.1.100.2", 'netmask': "24"}
-}
+router_vars = {'routers': {
+    "nxos1": {'interface': "Ethernet1/1",
+              'ip': "10.1.100.1",
+              'netmask': "24",
+              'peer_ip': "10.1.100.2"},
+    "nxos2": {'interface': "Ethernet1/1",
+              'ip': "10.1.100.2",
+              'netmask': "24",
+              'peer_ip': "10.1.100.1"}},
+    'local_as': 22}
 
-template_file = 'nxos_ipv4_intf.j2'
+template_file = 'nxos_intf_bgp.j2'
 template = env.get_template(template_file)
-output = template.render(router_vars=router_vars)
+output = template.render(**router_vars)
 print(output)
