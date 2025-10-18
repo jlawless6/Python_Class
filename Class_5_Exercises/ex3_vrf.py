1c578aba52f3bdef799fb6d5279719fa301b2006
Joel Lawless <jlawless6@gmail.com>
2025-10-18 13:46:12 -0700

    fixed ex3_vrf.py

--- Class_5_Exercises/ex3_vrf.py
+++ Class_5_Exercises/ex3_vrf.py
@@ -2,7 +2,7 @@ from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
 env = Environment(undefined=StrictUndefined)
-env.loader = FileSystemLoader("templates/exercise3/")
+env.loader = FileSystemLoader("./templates/exercise3/")
 
 j2_vars = {"vrf_name": "blue", "rd_number": "100:1", "ipv4_af": True, "ipv6_af": True}
 
