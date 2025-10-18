a6bef3baca908c97a61829aae18040e5403547e6
Joel Lawless <jlawless6@gmail.com>
2025-10-18 13:15:23 -0700

    Finished Class 5 Exercise 2C

--- Class_5_Exercises/ex2c_nxos_intf_configure.py
+++ Class_5_Exercises/ex2c_nxos_intf_configure.py
@@ -3,6 +3,8 @@ from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 import my_devices
 from netmiko import ConnectHandler
+import time
+import re
 
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader('./templates/exercise2')
@@ -10,67 +12,93 @@ env.loader = FileSystemLoader('./templates/exercise2')
 nxos1 = my_devices.inventory['nxos1']
 nxos2 = my_devices.inventory['nxos2']
 
-net_connect = ConnectHandler(**nxos1)
-output = net_connect.send_command("show ip interface brief")
 
-print(output)
+template_file = "nxos_intf_bgp.j2"
+interface = "Ethernet1/3"
 
-# template_file = 'nxos_intf_bgp.j2'
-# template = env.get_template(template_file)
-# output = template.render(**router_vars)
-# print(output)
+nxos1_vars = {
+    "device_name": "nxos1",
+    "local_as": 22,
+    "interface": interface,
+    "ipv4_address": "10.1.100.1",
+    "ipv4_netmask": "24",
+}
+
+nxos2_vars = {
+    "device_name": "nxos2",
+    "local_as": 22,
+    "interface": interface,
+    "ipv4_address": "10.1.100.2",
+    "ipv4_netmask": "24",
+}
 
 
+nxos1_vars["peer_ip"] = nxos2_vars["ipv4_address"]
+nxos2_vars["peer_ip"] = nxos1_vars["ipv4_address"]
 
 
+nxos1["j2_vars"] = nxos1_vars
+nxos2["j2_vars"] = nxos2_vars
 
+print()
+for device in (nxos1, nxos2):
+    # Create a copy as I am going to modify the dictionary
+    tmp_device = device.copy()
+    j2_vars = tmp_device.pop("j2_vars")
+    template = env.get_template(template_file)
+    cfg = template.render(**j2_vars)
+    device_name = device["j2_vars"]["device_name"]
+    print(f" {device_name} ".center(80, "#"))
+    print(f"\n>>> Template output {device_name}")
+    print(cfg)
+    # Stripping out whitespace will make CLI cfg-output display cleaner
+    cfg_lines = [cfg.strip() for cfg in cfg.splitlines()]
 
+    # Establish Netmiko Connection
+    net_connect = ConnectHandler(**tmp_device)
+    # Store the SSH connection for later (so I don't have to reconnect)
+    device["ssh_conn"] = net_connect
+    print(f">>> Configuring {device_name}")
+    output = net_connect.send_config_set(cfg_lines)
+    print(output)
+    print("\n\n")
 
+# Give BGP enough time to reach the established state
+sleep_time = 15
+print(f"Sleeping for {sleep_time} seconds...")
+time.sleep(sleep_time)
 
+print("\n\n")
+print(">>> Testing ping and BGP")
+for device in (nxos1,):
+    net_connect = device["ssh_conn"]
+    remote_ip = device["j2_vars"]["peer_ip"]
 
-# #######################################################
+    # Test ping
+    output = net_connect.send_command(f"ping {remote_ip}")
+    print(output)
+    if "64 bytes from" not in output:
+        print("\nPing failed!!!")
+    print("\n\n")
 
-# from netmiko import ConnectHandler
-# import yaml
-# import os
-# from datetime import datetime
+    # Test BGP
+    bgp_verify = f"show ip bgp summary | include {remote_ip}"
+    output = net_connect.send_command(bgp_verify)
+    # Retrieve the State/PfxRcd field which is the last field
+    match = re.search(r"\s+(\S+)\s*$", output)
+    prefix_received = match.group(1)
+    try:
+        # If this is an integer, the BGP session reached the established state
+        int(prefix_received)
+        print(
+            f"BGP reached the established state. Prefixes received {prefix_received}"
+        )
+    except ValueError:
+        print("BGP failed to reach the established state")
 
-# home_dir = os.path.expanduser("~")
+# All done - disconnect on both devices
+for device in (nxos1, nxos2):
+    net_connect = device["ssh_conn"]
+    net_connect.disconnect()
 
-# def load_yaml_to_dict(file_path):
-#     with open(file_path, 'r') as f:
-#         try:
-#             data = yaml.safe_load(f)
-#             return data
-#         except yaml.YAMLError as e:
-#             print(f"Error loading YAML file: {e}")
-#             return None
-
-# filename = ".netmiko.yml"
-# inventory = load_yaml_to_dict(os.path.join(home_dir, filename))
-
-# start_time = datetime.now()
-
-# net_connect = ConnectHandler(**inventory['cisco3'], fast_cli=True)
-
-# commands = ["ip name-server 1.1.1.1", "ip name-server 1.0.0.1", "ip domain-lookup"]
-
-# output = net_connect.send_config_set(commands)
-
-# print()
-# print("*" * 80)
-# print(output)
-# print("*" * 80)
-# print()
-
-# ping_result = net_connect.send_command("ping 8.8.8.8")
-
-# if "!!!!!" in ping_result:
-#     print("Ping successful")
-# else:
-#     print("Ping failed")
-
-# net_connect.disconnect()
-
-# end_time = datetime.now()
-# print(f"Total time: {end_time - start_time}")
+print("\n\n")
