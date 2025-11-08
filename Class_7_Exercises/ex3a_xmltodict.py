db6e7c118268796a487910c79046e5c4aac8c5a6
Joel Lawless <jlawless6@gmail.com>
2025-11-08 11:31:05 -0800

    Completed Class 7 Exercise 3abc on ex3a

--- Class_7_Exercises/ex3a_xmltodict.py
+++ Class_7_Exercises/ex3a_xmltodict.py
@@ -12,9 +12,18 @@ def read_xml_file(file_name):
     with open(os.path.join(cwd, file_name), 'r') as f:
         return xmltodict.parse(f.read())
 
+def read_xml_file_forcelist(file_name, force_list=None):
+    with open(os.path.join(cwd, file_name), 'r') as f:
+        if force_list is None:
+            force_list = {}
+        else:
+            force_list = {force_list: True}
+        data = xmltodict.parse(f.read(), force_list=force_list)
+        return data
+
 sec_zones = read_xml_file('show_security_zones.xml')
 
-sec_zone_single = read_xml_file('show_security_zones_single_trust.xml')
+sec_zone_single = read_xml_file_forcelist('show_security_zones_single_trust.xml', 'zones-security')
 
 
 pprint(sec_zones)
